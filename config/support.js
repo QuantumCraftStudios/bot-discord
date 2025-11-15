30d0ef6e68eab358eeda74ba702137cf37b90ff0
UltraLionFr <ultralionfr@gmail.com>
2025-11-15 04:56:36 +0100

    chore: Ajout du fichier support.js

---
+++ config/support.js
@@ -0,0 +1,48 @@
+export const supportConfig = {
+    ticketUrl: "https://clients.quantumcraft-studios.com/submitticket.php",
+
+    categories: [
+        {
+            emoji: "💬",
+            title: "Support général",
+            description:
+                'Vous avez des questions qui ne sont pas traitées par d\'autres services ? Contactez notre service "Renseignements généraux" pour toute question ou assistance non spécialisée.',
+            url: "https://clients.quantumcraft-studios.com/submitticket.php?step=2&deptid=1",
+        },
+        {
+            emoji: "🔧",
+            title: "Support technique",
+            description:
+                "Vous rencontrez un problème technique avec l'un de vos services ? Contactez notre service technique afin d'obtenir une assistance.",
+            url: "https://clients.quantumcraft-studios.com/submitticket.php?step=2&deptid=2",
+        },
+        {
+            emoji: "🚨",
+            title: "Support d'urgence",
+            description:
+                "Contactez notre \"Support d'urgence\" 24/7 pour les problèmes d'hébergement urgents tels que les pannes, les failles de sécurité et les pertes de données. Nous sommes là pour résoudre rapidement ces incidents critiques.",
+            url: "https://clients.quantumcraft-studios.com/submitticket.php?step=2&deptid=3",
+        },
+        {
+            emoji: "💳",
+            title: "Service facturation",
+            description:
+                'Pour toute question relative aux paiements, aux factures et aux comptes, adressez-vous à notre service "Facturation" qui vous apportera un soutien rapide.',
+            url: "https://clients.quantumcraft-studios.com/submitticket.php?step=2&deptid=4",
+        },
+        {
+            emoji: "💼",
+            title: "Service commercial",
+            description:
+                'Vous envisagez un achat ou vous avez besoin d\'informations sur un produit ? Le service "commercial" est votre interlocuteur privilégié pour explorer nos offres et prendre des décisions en connaissance de cause.',
+            url: "https://clients.quantumcraft-studios.com/submitticket.php?step=2&deptid=5",
+        },
+        {
+            emoji: "🎨",
+            title: "Projet sur mesure",
+            description:
+                "Vous recherchez des solutions sur mesure ou des projets uniques ? Contactez notre service de demande de projets personnalisés pour discuter de vos besoins spécifiques et de vos idées de projets.",
+            url: "https://clients.quantumcraft-studios.com/submitticket.php?step=2&deptid=6",
+        },
+    ],
+};
