1b80eac90243f447a4dc4fef5a563862c72bacc0
UltraLionFr <ultralionfr@gmail.com>
2025-11-15 03:37:25 +0100

    chore: Ajout du dossier config qui contient services.js (liens des services de quantumcraft-studios.com)

---
+++ config/services.js
@@ -0,0 +1,17 @@
+export const services = [
+    {
+        name: "Site Vitrine",
+        url: "https://quantumcraft-studios.com",
+        emoji: "🌐",
+    },
+    {
+        name: "Manager",
+        url: "https://manager.quantumcraft-studios.com",
+        emoji: "🖥️",
+    },
+    {
+        name: "Serveur Mail",
+        url: "https://mail.qcstd.com",
+        emoji: "📧",
+    },
+];
