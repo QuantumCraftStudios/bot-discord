ff40afde5e1827e3ad9289cc43cc3fffd8b670a1
UltraLionFr <ultralionfr@gmail.com>
2025-11-15 04:12:46 +0100

    chore: Ajout du fichier liens.js

---
+++ config/liens.js
@@ -0,0 +1,13 @@
+export const liens = {
+    rules: "1371889547368988784",
+    status: "https://uptime.quantumcraft-studios.com/",
+    docs: "https://docs.quantumcraft-studios.com/",
+    legal: "https://quantumcraft-studios.com/mentions-legales",
+    rgpd: "https://quantumcraft-studios.com/rgpd",
+    cgv: "https://quantumcraft-studios.com/cgv",
+    cgu: "https://quantumcraft-studios.com/cgu",
+    contact: "https://quantumcraft-studios.com/contact",
+    website: "https://quantumcraft-studios.com",
+    trustpilot: "https://www.trustpilot.com/review/quantumcraft-studios.com",
+    twitter: "https://x.com/QuantumCraftStd",
+};
